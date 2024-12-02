f10fcd8036b8980600a4fccc6cdc5bf6fcd2e9f5
Arun kumar <arun987604@gmail.com>
2024-12-02 23:45:30 +0530

    permission fix

--- src/test/java/com/arun/sims/SimsApplicationTests.java
+++ src/test/java/com/arun/sims/SimsApplicationTests.java
@@ -3,11 +3,8 @@ package com.arun.sims;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SimsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
