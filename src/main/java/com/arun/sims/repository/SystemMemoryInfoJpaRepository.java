683e6414a2ba4231bcf0dbe1b405d5d81c8b2276
Arun kumar <arun987604@gmail.com>
2024-12-20 21:42:54 +0530

    restructure project

--- src/main/java/com/arun/sims/repository/SystemMemoryInfoJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.arun.sims.repository;
-
-import com.arun.sims.model.SystemMemoryInfo ;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SystemMemoryInfoJpaRepository extends JpaRepository<SystemMemoryInfo , Long> {
-}
--- src/test/java/com/arun/sims/SimsApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.arun.sims;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-class SimsApplicationTests {
-}
